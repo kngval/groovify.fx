1af5ce2a74ad8ada4f98e6760bcc3748da8f166f
kngval <bkvalteocs@gmail.com>
2024-06-08 16:00:13 +0800

    changed name

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="p-2 bg-slate-500 flex justify-center items-center">
       <div className="w-[300px] h-12 bg-slate-400">
         <div className="nav-title font-extrabold flex items-center">
-          groovify
+          groovify TESTING NEW BRANCH
         </div>
       </div>
     </div>
